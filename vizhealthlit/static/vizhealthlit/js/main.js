acab57281a058bcecf2d7b27dc629ff98dfce190
Nick Reid <nickreid@nickreid.com>
2014-06-16 16:56:22 -0400

    basic d3 barchart

--- vizhealthlit/static/vizhealthlit/js/main.js
+++ vizhealthlit/static/vizhealthlit/js/main.js
@@ -12,7 +12,21 @@ $.ajaxSetup({
     }
 });
 
+$(window).resize(function(){
+	var form = $('form');
+	form.css({
+		float:'left',
+	});
+	var chart = $('#chart');
+	chart.css({
+		float:'left',
+		height:form.height(),
+		width:form.parent().width() - form.width(),
+	});
+});
+
 $(document).ready(function(){
+	$(window).resize();
 	$('form').submit(function(event){
 		event.preventDefault();
 		var form = $(this);
@@ -20,9 +34,27 @@ $(document).ready(function(){
 			url: form.attr("action"),
 			type: form.attr("method"),
 			data:form.serializeArray(),
-			done:function(data){
-				alert(data);
+			success:function(data){
+				draw_streams(data['words']['meanings']);
 			},
 		});
 	});
 });
+
+function draw_streams(data){
+	var chart=$("#chart");
+	chart.html("");
+
+	var svg = d3.select("#chart").append("svg:svg")
+		.attr("width", chart.width())
+		.attr("height", chart.height());
+
+	var num = 0;
+	var rect = svg.selectAll("rect")
+	    .data(data)
+	  .enter().append("rect")
+	    .attr("x", function(d) { num += 1; return num; })
+	    .attr("y", 0)
+	    .attr("width", 1)
+	    .attr("height", function(d){ return d; });
+}
