eefedd22504beb99b17d5762409dc780fee9dacb
Nick Reid <nickreid@nickreid.com>
2014-07-10 17:45:28 -0400

    got bars to stack correctly

--- vizhealthlit/static/vizhealthlit/js/bars.js
+++ vizhealthlit/static/vizhealthlit/js/bars.js
@@ -1,5 +1,7 @@
-function draw_bars(data){
-	var barHeight=20;
+function draw_bars(items){
+
+	var display_paragraphs = true;
+
 	var color = d3.scale.category10();
 
 	var chart = $("#chart");
@@ -7,26 +9,51 @@ function draw_bars(data){
 		.attr("width", chart.width())
 		.attr("height", chart.height());
 
-	var x = d3.scale.linear().domain([
-		d3.min(data,function(d){ return d.length; }),
-		d3.max(data, function(d){ return d.length; })
+	var y = d3.scale.linear().domain([
+			0,
+			d3.max(items, function(d){ return d.length; })
 		]).range([
 			0,
 			chart.height()/2
 		]);
 	
-	var bars = svg.selectAll("g").data(data).enter().append("g")
-		.attr("transform", function(d,i){
-		return "translate(0,"+barHeight*i+")";
+	var sentences = []
+	items.forEach(function(d){
+		d.sentences.forEach(function(s){
+			s.paragraph = d;
+			sentences.push(s);
+		});
 	});
-	bars.append("rect").attr({
-		height:barHeight,
-		width:function(d){
-			return x(d.length);
+	var barWidth = chart.width()/sentences.length;
+	var items = svg.selectAll("g").data(items).enter().append("g");
+
+
+	items.selectAll("rect").data(function(d){
+		return d.sentences;
+	}).enter().append("rect").attr({
+		width:barWidth,
+		height:function(d){
+			return y(d.length);
 		},
 		fill:function(d){
 			return color(d.score);
 		},
+	});
+	items[0].forEach(function(d){
+		ypos=0;
+		xpos=0;
+		d3.select(d).selectAll("rect").attr({
+			x:function(d){
+				if(display_paragraphs) return 0;
+				xpos += barWidth;
+				return xpos - barWidth;
+			},
+			y:function(d){
+				if(!display_paragraphs) return this.getBBox().height/2;
+				ypos += this.getBBox().height;
+				return ypos - this.getBBox().height;
+			}
+		})
 	});
 }
 
