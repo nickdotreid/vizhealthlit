f7610991147f3fd257e3bdc3af6776b149024028
Nick Reid <nickreid@nickreid.com>
2014-07-21 17:52:44 -0400

    got pizza chart working pretty well

--- vizhealthlit/static/vizhealthlit/js/pizza.js
+++ vizhealthlit/static/vizhealthlit/js/pizza.js
@@ -4,6 +4,20 @@ function draw_pizza(items, settings){
 		.attr("width", chart.width())
 		.attr("height", chart.height());
 
+	var sentences = []
+	items.forEach(function(d){
+		d.sentences.forEach(function(s){
+			s.paragraph = d;
+			sentences.push(s);
+		});
+	});
+
+	var color = d3.scale.linear()
+	.domain([
+		d3.min(sentences,function(d){ return d.score; }),
+		d3.max(sentences,function(d){ return d.score; })
+	]).range(['#3c783e','#00ea13']);
+
 	var totalLength = 0;
 	items.forEach(function(d){
 		totalLength += d.words.length;
@@ -11,18 +25,67 @@ function draw_pizza(items, settings){
 
 	var degree = d3.scale.linear()
 	.domain([0, totalLength])
-	.range([0,360]);
+	.range([0,(Math.PI * 2)]);
 
-	var radius = d3.scale.linear()
-	.domain([0,totalLength])
-	.range([0, function(){
-		if(chart.width() > chart.height()) return chart.height()/4;
-		return chart.width()/4
-	}])
 
-	svg.selectAll("g.wedge").data(items)
+	var highestScore = d3.max(items, function(d){
+		return d.score;
+	});
+	function scoreJitter(d){
+		var s = d.score/highestScore;
+		if(s > 0.8) return 0;
+		return 1-s;
+	}
+
+	var dPos = 0;
+
+	var container = svg.append("g");
+	container.attr("transform","translate("+(chart.width()/2)+","+(chart.height()/2)+")");
+	var paragraphs = container.selectAll("g.wedge").data(items)
 	.enter().append("g").attr("class","wedge")
+	.each(function(d){
 
+		var r = chart.height()/4;
+
+		var rPos = 0;
+		var paragraphFraction = d.words.length/totalLength;
+		var paragraphArea = Math.PI * r * r * paragraphFraction;
+		var paragraphVal = d.words.length;
+		var currentVal = 0;
+
+		var start = degree(dPos);
+		dPos += d.words.length;
+		var end = degree(dPos);
+
+		var arc = d3.svg.arc()
+		.innerRadius(function(){
+			return rPos;
+		})
+		.outerRadius(function(d){
+			currentVal += d.words.length;
+			var currentArea = paragraphArea * (currentVal/paragraphVal);
+			rPos = Math.sqrt((currentArea*(1/paragraphFraction))/Math.PI);
+			return rPos;
+		})
+		.startAngle(start)
+		.endAngle(end);
+
+		d3.select(this).attr("transform",function(){
+			var distance = r/4 * scoreJitter(d);
+			var angle = start + (end - start)/2;
+			var xpos = Math.sin(angle) * distance;
+			var ypos = Math.cos(angle) * distance * -1;
+			return "translate("+xpos+","+ypos+")";
+		}).selectAll("path").data(function(){
+			return d.sentences;
+		}).enter()
+		.append("path").attr("d",arc)
+		.style("fill",function(d){
+			return color(d.score);
+		});
+	});
+
+	var sentences = paragraphs.selectAll("path");
 }
 
 
