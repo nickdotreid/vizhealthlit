f3b693c3bf4b69474bf6af9c6763b0e82dadbfaa
Nick Reid <nickreid@nickreid.com>
2014-08-06 16:45:31 -0400

    seperated settings and text form, so they can be displayed seperately

--- vizform/views.py
+++ vizform/views.py
@@ -18,55 +18,55 @@ class TextForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(TextForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
+
         self.helper.form_method = "POST"
         self.helper.form_action = reverse(result)
 
-        self.helper.form_class = 'form-horizontal'
+        self.helper.form_class = 'form-horizontal text-form'
         self.helper.label_class = 'col-lg-2'
         self.helper.field_class = 'col-lg-8'
 
         self.helper.layout = Layout(
             'text',
-            'style',
             Submit('submit', 'Submit'),
             )
 
     text = forms.CharField(label="The text you want to visualize", required=True, widget=forms.Textarea)
-    style = forms.ChoiceField(label="Pick a layout", required=True, 
-        choices=(
-        ('bars','Bar Chart'),
-        ('tree','Tree Map'),
-        ('pizza','Pizza Chart'),
-        ))
 
-class SettingsForm(TextForm):
+class SettingsForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super(SettingsForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = "POST"
+        self.helper.form_action = reverse(result)
+
+        self.helper.form_class = 'form-vertical settings-form'
 
         self.helper.layout = Layout(
+            'text',
+            'style',
             Div(
-                Div(
-                    'text',
-                    'style',
-                    css_class="col-md-6"),
-                Div(
-                    Div(
-                        Div('sentences_threshold_min', css_class="col-md-6"),
-                        Div('sentences_threshold_max', css_class="col-md-6"),
-                        css_class="row"
-                        ),
-                    Div(
-                        Div('words_threshold_min', css_class="col-md-6"),
-                        Div('words_threshold_max', css_class="col-md-6"),
-                        css_class="row"
-                    ),
-                    'sentences_threshold',
-                    'negativity_threshold',
-                    'correct_percent',
-                    css_class="col-md-6"),
-                css_class="row"),
+                Div('sentences_threshold_min', css_class="col-xs-6"),
+                Div('sentences_threshold_max', css_class="col-xs-6"),
+                css_class="row"
+                ),
+            Div(
+                Div('words_threshold_min', css_class="col-xs-6"),
+                Div('words_threshold_max', css_class="col-xs-6"),
+                css_class="row"
+            ),
+            'sentences_threshold',
+            'negativity_threshold',
+            'correct_percent',
             Submit('submit', 'Submit'),
             )
+
+    style = forms.ChoiceField(label="Pick a layout", required=True, 
+            choices=(
+            ('bars','Bar Chart'),
+            ('tree','Tree Map'),
+            ('pizza','Pizza Chart'),
+            ))
 
     words_threshold_min = forms.CharField(required=False, label="Min", initial=8)
     words_threshold_max = forms.CharField(required=False, label="Max", initial=10)
@@ -80,7 +80,8 @@ class SettingsForm(TextForm):
 
 def index(request):
     return render_to_response('index.html',{
-        'form':SettingsForm(),
+        'form':TextForm(),
+        'settings_form':SettingsForm(),
         },context_instance=RequestContext(request))
 
 def result(request):
