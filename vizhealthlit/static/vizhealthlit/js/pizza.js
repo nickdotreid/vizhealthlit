5da0408b550fe7a4d4a91ea1b4fba42fe7d84ca9
Nick Reid <nickreid@nickreid.com>
2014-08-06 18:47:36 -0400

    stuffed everything into the blit function to allow for updateing

--- vizhealthlit/static/vizhealthlit/js/pizza.js
+++ vizhealthlit/static/vizhealthlit/js/pizza.js
@@ -38,6 +38,55 @@ function draw_pizza(items, settings){
 		return 1-s;
 	}
 
+	function makeArcs(d,r,start,end){
+		var rPos = 0;
+		var paragraphFraction = d.words.length/totalLength;
+		var paragraphArea = Math.PI * r * r * paragraphFraction;
+		var paragraphVal = d.words.length;
+		var currentVal = 0;
+
+		return d3.svg.arc()
+		.innerRadius(function(){
+			return rPos;
+		})
+		.outerRadius(function(d){
+			currentVal += d.words.length;
+			var currentArea = paragraphArea * (currentVal/paragraphVal);
+			rPos = Math.sqrt((currentArea*(1/paragraphFraction))/Math.PI);
+			return rPos;
+		})
+		.startAngle(start)
+		.endAngle(end);
+	}
+
+	function makeColors(items, settings){
+		var hue = 115;
+		var brightnessKey = 'score';
+		var saturationKey = 'score';
+
+		if(settings['words'] && settings['words'].length > 0){
+			hue = 21;
+			saturationKey = 'wordScore';
+		}
+
+		var brightness = d3.scale.linear()
+		.domain([
+			d3.min(items,function(d){ return d[brightnessKey]; }),
+			d3.max(items,function(d){ return d[brightnessKey]; })
+		]).range([20,80]);
+		
+		var saturation = d3.scale.linear()
+		.domain([
+			d3.min(items,function(d){ return d[saturationKey]; }),
+			d3.max(items,function(d){ return d[saturationKey]; })
+		]).range([0,100]);
+
+		return function(d){
+			var cstr = "hsl("+hue+","+saturation(d[saturationKey])+"%,"+brightness(d[brightnessKey])+"%)";
+			return d3.hsl(cstr).toString();
+		}
+	}
+
 	var lPos = 0;
 	var rPos = 0;
 	var goingRight = true;
@@ -68,83 +117,21 @@ function draw_pizza(items, settings){
 		}
 	}
 
-	items.sort(function(a,b){
-		if(a.score < b.score){
-			return -1;
-		}else if(a.score > b.score){
-			return 1;
-		}
-		return 0;
-	}).reverse();
-
 	var container = svg.append("g");
 	container.attr("transform","translate("+(chart.width()/2)+","+(chart.height()/2)+")");
-	var paragraphs = container.selectAll("g.wedge").data(items)
-	.enter().append("g").attr("class","wedge")
-	.each(function(d){
 
-		var r = chart.height()/4;
-
-		$(this).hover(function(){
-			showTooltip(d);
-		},function(){
-//			hideTooltip(d);
-		});
-
-		var rPos = 0;
-		var paragraphFraction = d.words.length/totalLength;
-		var paragraphArea = Math.PI * r * r * paragraphFraction;
-		var paragraphVal = d.words.length;
-		var currentVal = 0;
-
-		var arcPos = arcPosition(degree(d.words.length));
-		var start = arcPos['start'];
-		var end = arcPos['end'];
-
-		var arc = d3.svg.arc()
-		.innerRadius(function(){
-			return rPos;
-		})
-		.outerRadius(function(d){
-			currentVal += d.words.length;
-			var currentArea = paragraphArea * (currentVal/paragraphVal);
-			rPos = Math.sqrt((currentArea*(1/paragraphFraction))/Math.PI);
-			return rPos;
-		})
-		.startAngle(start)
-		.endAngle(end);
-
-		d3.select(this).attr("transform",function(){
-			var distance = r/4 * scoreJitter(d);
-			var angle = start + (end - start)/2;
-			var xpos = Math.sin(angle) * distance;
-			var ypos = Math.cos(angle) * distance * -1;
-			return "translate("+xpos+","+ypos+")";
-		}).selectAll("path").data(d.sentences).enter()
-		.append("path").attr("d",arc)
-		.style("fill",function(d){
-			return color(d.score);
-		}).each(function(d){
-			var timeout = false;
-			$(this).hover(function(){
-				timeout = setTimeout(function(){
-					timeout = false;
-					hoverSentence(d);
-				},150);
-			}, function(){
-				if(timeout){
-					clearTimeout(timeout);
-				}else{
-					clearSentence(d);
-				}
-			});
-		});
-	});
-
-	var sentenceElements = paragraphs.selectAll("path");
 	var originalColor = color;
-	return function blit(items, settings){
+	function blit(items, settings){
 		// update data
+		items.sort(function(a,b){
+			if(a.score < b.score){
+				return -1;
+			}else if(a.score > b.score){
+				return 1;
+			}
+			return 0;
+		}).reverse();
+
 		var sentences = []
 		items.forEach(function(d){
 			d.sentences.forEach(function(s){
@@ -152,46 +139,64 @@ function draw_pizza(items, settings){
 				sentences.push(s);
 			});
 		});
-		paragraphs.selectAll("g.wedge").data(items).each(function(d){
-			d3.select(this).selectAll("path").data(d.sentences);
-		});
 
 		var color = makeColors(sentences, settings);
 
+		container.selectAll("g.wedge").data(items)
+		.enter().append("g").attr("class","wedge");
+		container.selectAll("g.wedge").data(items).exit().remove();
+
 		// update elements
-		sentenceElements.style("fill",function(d){
-			return color(d);
+		container.selectAll("g.wedge").each(function(d){
+			var r = chart.height()/2;
+
+			var arc = d3.svg.arc()
+			.innerRadius(0)
+			.outerRadius(0)
+			.startAngle(0)
+			.endAngle(0);
+
+			d3.select(this).selectAll("path")
+			.data(d.sentences)
+			.enter()
+			.append("path").attr("d",arc)
+			.style("fill","#FFFFFF")
+			.each(function(d){
+				/*** ADDS HOVER STATE FOR EACH SENTENCE ****/
+				var timeout = false;
+				$(this).hover(function(){
+					timeout = setTimeout(function(){
+						timeout = false;
+						hoverSentence(d);
+					},150);
+				}, function(){
+					if(timeout){
+						clearTimeout(timeout);
+					}else{
+						clearSentence(d);
+					}
+				});
+			})
+			d3.select(this).selectAll("path").data(d.sentences).exit().remove();
+
+			var arcPos = arcPosition(degree(d.words.length));
+			var start = arcPos['start'];
+			var end = arcPos['end'];
+
+			arc = makeArcs(d,r, start, end);
+
+			d3.select(this).selectAll("path")
+			.attr("d",arc)
+			.style("fill",function(d){
+				return color(d);
+			});
 		});
 	}
+	blit(items,settings);
+	return blit;
 }
 
-function makeColors(items, settings){
-		var hue = 115;
-		var brightnessKey = 'score';
-		var saturationKey = 'score';
 
-		if(settings['words'] && settings['words'].length > 0){
-			hue = 21;
-			saturationKey = 'wordScore';
-		}
-
-		var brightness = d3.scale.linear()
-		.domain([
-			d3.min(items,function(d){ return d[brightnessKey]; }),
-			d3.max(items,function(d){ return d[brightnessKey]; })
-		]).range([20,80]);
-		
-		var saturation = d3.scale.linear()
-		.domain([
-			d3.min(items,function(d){ return d[saturationKey]; }),
-			d3.max(items,function(d){ return d[saturationKey]; })
-		]).range([0,100]);
-
-		return function(d){
-			var cstr = "hsl("+hue+","+saturation(d[saturationKey])+"%,"+brightness(d[brightnessKey])+"%)";
-			return d3.hsl(cstr).toString();
-		}
-}
 
 
 visualization_functions['pizza'] = draw_pizza;
