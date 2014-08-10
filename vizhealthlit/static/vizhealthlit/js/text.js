b7930897ad460fbd6a2998cde7df7aa11137b677
Nick Reid <nickreid@nickreid.com>
2014-08-10 14:19:22 -0400

    integrated in functions

--- vizhealthlit/static/vizhealthlit/js/text.js
+++ vizhealthlit/static/vizhealthlit/js/text.js
@@ -1,5 +1,96 @@
 var TextModel = Backbone.Model.extend({
+	initialize:function(){
+		var model = this;
+		this.on("change:items",function(){
+			model.generateScores();
+		});
+	},
+	getSentences: function(){
+		var items = this.get("items");
+		if(!items) return [];
 
+		var sentences = [];
+		items.forEach(function(d){
+			d.sentences.forEach(function(s){
+				s.paragraph = d;
+				sentences.push(s);
+			});
+		});
+		return sentences;
+	},
+	getSettings: function(){
+		return {
+			formula:this.get("formula"),
+		};
+	},
+	generateScores: function(){
+		var sentences = this.getSentences();
+		var settings = this.getSettings();
+		var items = this.get("items");
+		if(!items) return false;
+		if(settings['formula'] == 'custom'){
+			if(!settings['sentences_threshold_min']) settings['sentences_threshold_min'] = d3.min(items,function(d){ return d.sentences.length; });
+			if(!settings['sentences_threshold_max']) settings['sentences_threshold_max'] = d3.max(items,function(d){ return d.sentences.length; });
+
+			if(!settings['words_threshold_min']) settings['words_threshold_min'] = d3.min(sentences,function(d){ return d.words.length; });
+			if(!settings['words_threshold_max']) settings['words_threshold_max'] = d3.max(sentences,function(d){ return d.words.length; });
+
+			
+			function score(d, type){
+				d.score = 1;
+
+				var length_max = settings[type+'_threshold_max']
+				var length_min = settings[type+'_threshold_min']
+
+				var length = d.words.length;
+				if(d.sentences) length = d.sentences.length;
+				
+				if(length < length_min){
+					d.score += length - length_min;
+				}
+				if(length > length_max){
+					d.score += length_max - length;
+				}
+
+				if(d.sentences) d.sentences.forEach(function(s){ d.score += s.score; });
+
+				d.score += d.active_words - d.passive_words;
+			}
+		}else if(items[0][settings['formula']]){
+			function score(d, type){
+				d.score = d[settings['formula']];
+			}
+		}else{
+			return;
+		}
+
+		function wordScore(d){
+			d.wordScore = 0;
+			if(!settings.words || settings.words.length < 1) return;
+			d.words.forEach(function(w){
+				if(settings.words.indexOf(w) >= 0) d.wordScore++;
+			});
+		}
+
+		items.forEach(function(d){
+			d.sentences.forEach(function(s){
+				score(s,'words');
+				wordScore(s);
+			});
+			score(d,'sentences');
+			wordScore(d);
+		});
+
+		var items_max = d3.max(items, function(d){ return d.score; });
+		var sentences_max = d3.max(items, function(d){ return d.score; });
+
+		items.forEach(function(d){
+			if(d.score > items_max) d.score = items_max;
+			d.sentences.forEach(function(s){ if(s.score > sentences_max) s.score = sentences_max; });
+		});
+
+		return items;
+	}
 });
 
 var UploadView = Backbone.View.extend({
@@ -59,17 +150,17 @@ var TextView = Backbone.View.extend({
 		if(item.sentences.length == 1){
 			view.$(".score:last").addClass("paragraph-score");
 		}else{
-			var scoreAttribute = this.model.get("scoring");
-			$(".score:last",view.$el).after('<span class="score paragraph-score">('+formatNumber(item[scoreAttribute]) + ')</span>');
+			var formula = this.model.get("formula");
+			$(".score:last",view.$el).after('<span class="score paragraph-score">('+formatNumber(item[formula]) + ')</span>');
 		}
 	},
 	scoreSentence: function(item){
 		var html = this.$el.html();
 		var parts = html.split(item.text);
 
-		var scoreAttribute = this.model.get("scoring");
+		var formula = this.model.get("formula");
 		if(parts[0]) this.$el.html(parts[0]);
-		this.$el.html(this.$el.html() + item.text + "<span class='score'>"+ "("+ formatNumber(item[scoreAttribute]) +")" +"</span>");
+		this.$el.html(this.$el.html() + item.text + "<span class='score'>"+ "("+ formatNumber(item[formula]) +")" +"</span>");
 		if(parts[1]) this.$el.html(this.$el.html() + parts[1]);
 		return true;
 	},
@@ -81,18 +172,18 @@ function formatNumber(num){
 
 var SettingsView = Backbone.View.extend({
 	events:{
-		"click .scoring a":"scoreUpdate",
+		"click .formulas a":"scoreUpdate",
 	},
 	render:function(event){
 		return this;
 	},
 	scoreUpdate: function(event){
 		event.preventDefault();
-		this.$(".scoring .active").removeClass("active");
+		this.$(".formulas .active").removeClass("active");
 		var button = $(event.currentTarget).parents("li:first");
 		button.addClass("active");
 		this.model.set({
-			'scoring':button.data("score"),
+			'formula':button.data("formula"),
 		});
 	}
 })
