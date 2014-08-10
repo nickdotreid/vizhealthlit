aef272594cdaba2fbc9be6405bbcf8cb4e3f3c01
Nick Reid <nickreid@nickreid.com>
2014-08-10 12:46:56 -0400

    added view to show scores for items

--- vizhealthlit/static/vizhealthlit/js/text.js
+++ vizhealthlit/static/vizhealthlit/js/text.js
@@ -44,10 +44,40 @@ var TextView = Backbone.View.extend({
 		this.listenTo(this.model, "change", this.render);
 	},
 	render: function(){
+		var view = this;
 		this.$el.html(this.model.get("text"));
+		this.model.get("items",[]).forEach(function(item){
+			view.scoreGroup(item);
+		});
 		return this;
-	}
+	},
+	scoreGroup: function(item){
+		var view = this;
+		item.sentences.forEach(function(sentence){
+			view.scoreSentence(sentence);
+		});
+		if(item.sentences.length == 1){
+			view.$(".score:last").addClass("paragraph-score");
+		}else{
+			var scoreAttribute = this.model.get("scoring");
+			$(".score:last",view.$el).after('<span class="score paragraph-score">('+formatNumber(item[scoreAttribute]) + ')</span>');
+		}
+	},
+	scoreSentence: function(item){
+		var html = this.$el.html();
+		var parts = html.split(item.text);
+
+		var scoreAttribute = this.model.get("scoring");
+		if(parts[0]) this.$el.html(parts[0]);
+		this.$el.html(this.$el.html() + item.text + "<span class='score'>"+ "("+ formatNumber(item[scoreAttribute]) +")" +"</span>");
+		if(parts[1]) this.$el.html(this.$el.html() + parts[1]);
+		return true;
+	},
 });
+
+function formatNumber(num){
+	return Math.round(num *100)/100;
+}
 
 var SettingsView = Backbone.View.extend({
 	events:{
