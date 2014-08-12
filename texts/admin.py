0a83a38b6f9f6da29968885de78b5ba2bc96dd37
Nick Reid <nickreid@nickreid.com>
2014-08-12 11:44:20 -0400

    made form use textareas

--- texts/admin.py
+++ texts/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from texts.models import Text
 
-admin.site.register(Text)
+from django import forms
+
+class TextModelForm( forms.ModelForm):
+	text = forms.CharField( widget=forms.Textarea )
+	class Meta:
+		model = Text
+
+class TextAdmin(admin.ModelAdmin):
+	form = TextModelForm
+
+admin.site.register(Text, TextAdmin)
